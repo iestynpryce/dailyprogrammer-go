b1ce5d257af01e2a45e611624059045f59000939
Iestyn Pryce <iestyn.pryce@gmail.com>
2015-05-08 22:09:41 +0100

    Correct o/p as per https://gist.githubusercontent.com/jcbelanger/7b7bacb5afe40aff4173/raw/Hex%20Pronunciation

--- 213/e213/e213.go
+++ 213/e213/e213.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-var m = map[string]string{
+var first_byte = map[string]string{
 	"1": "one",
 	"2": "two",
 	"3": "three",
@@ -17,39 +17,126 @@ var m = map[string]string{
 	"7": "seven",
 	"8": "eight",
 	"9": "nine",
-	"A": "atta",
-	"B": "bibbity",
-	"C": "city",
-	"D": "dickety",
-	"E": "ebbity",
-	"F": "fleventy",
+	"A": "a",
+	"B": "bee",
+	"C": "cee",
+	"D": "dee",
+	"E": "ee",
+	"F": "eff",
+}
+
+var second_byte = map[string]string{
+	"10": "ten",
+	"11": "eleven",
+	"12": "twelve",
+	"13": "thirteen",
+	"14": "fourteen",
+	"15": "fifteen",
+	"16": "sixteen",
+	"17": "seventeen",
+	"18": "eighteen",
+	"19": "nineteen",
+	"20": "twenty",
+	"30": "thirty",
+	"40": "fourty",
+	"50": "fifty",
+	"60": "sixty",
+	"70": "seventy",
+	"80": "eighty",
+	"90": "ninety",
+	"A":  "atta",
+	"B":  "bibbity",
+	"C":  "city",
+	"D":  "dickety",
+	"E":  "ebbity",
+	"F":  "fleventy",
+}
+
+func pronounce_twochar(text string, i *int, d int) {
+	if strings.Contains("123456789", string(text[*i])) && d > 1 {
+		if text[*i] == '1' &&
+			strings.Contains("0123456789", string(text[*i+1])) {
+			fmt.Print(second_byte[text[*i:*i+2]])
+			*i++
+			if d > 3 {
+				fmt.Print(" bitey ")
+			}
+		} else {
+			fmt.Print(second_byte[string(text[*i])+"0"])
+			next := string(text[*i+1])
+			if next != "0" || d > 3 {
+				fmt.Print("-")
+			}
+		}
+	} else {
+		fmt.Print(second_byte[string(text[*i])])
+		if string(text[*i]) != "0" && string(text[*i+1]) != "0" {
+			fmt.Print("-")
+			fmt.Print(first_byte[string(text[*i+1])])
+			if d > 3 {
+				fmt.Print(" bitey ")
+			}
+			*i++
+		} else if d > 3 {
+			fmt.Print("-")
+		}
+	}
 }
 
 func pronounce_hex(text string) {
 	if strings.HasPrefix(text, "0x") { // check it's a hex value
-		fmt.Print(text + " \"")
-		for length, c := range text[2:] {
-			switch {
-			case length == 1 || length == 3 && c != '0':
-				fmt.Print("-")
-			case length == 2:
-				fmt.Print("bitey ")
+		fmt.Print(text + ": ")
+
+		subtext := text[2:]
+		nchars := len(subtext)
+
+		var last_zero bool = false
+
+		if subtext[0] == '0' {
+			last_zero = true
+		}
+
+		for i := 0; i < nchars; i++ {
+			d := nchars - i
+
+			if last_zero && subtext[i] == '0' {
+				continue
+			} else {
+				last_zero = false
 			}
-			if c != '0' {
-				trans := m[string(c)]
-				if length == 1 || length == 3 {
-					if !strings.Contains("onetwothreefourfivesixseveneightnine", trans) {
-						trans = strings.ToLower(string(c)) + "ee"
-					}
+
+			switch d {
+			case 4:
+				{
+					pronounce_twochar(subtext, &i, d)
 				}
-				fmt.Print(trans)
-				if length == 1 {
-					fmt.Print(" ")
+			case 3:
+				{
+					s := string(subtext[i])
+					fmt.Print(first_byte[s])
+					if s != "0" {
+						fmt.Print(" ")
+					}
+					fmt.Print("bitey ")
+				}
+			case 2:
+				{
+					pronounce_twochar(subtext, &i, d)
+				}
+			case 1:
+				{
+					s := string(subtext[i])
+					if s != "0" {
+						fmt.Print(first_byte[s])
+					}
 				}
 			}
 		}
-		fmt.Println("\"")
+		if last_zero {
+			fmt.Print("zero")
+		}
 	}
+	fmt.Println()
 }
 
 func main() {
